8cc70c244a72764f878d690c03666ee0bf47e4b8
Fivoryu <trevorfelixcalerosuyo@gmail.com>
2025-04-21 10:56:56 -0400

    modified some files
    i'm hungry

--- backend/academic_backend/academic_backend/wsgi.py
+++ backend/academic_backend/academic_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academic_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.academic_backend.academic_backend.settings')
 
 application = get_wsgi_application()
