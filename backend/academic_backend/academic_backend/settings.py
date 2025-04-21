da4e80a8d9225d22fa07fe5c429dbe9f9f94200b
Fivoryu <trevorfelixcalerosuyo@gmail.com>
2025-04-21 10:15:51 -0400

    zzz
    asdfaf

--- backend/academic_backend/academic_backend/settings.py
+++ backend/academic_backend/academic_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^p+k@gqh-qu^zr43xcc6bc(n_if*h0y-sg0-hnr9ih#p&6&bb0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
